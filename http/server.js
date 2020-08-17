607ceaa34b0a786a3b7a175affb38b8259b2063c
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-08-17 16:02:51 -0300

    Updating some files

---
+++ http/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+http
+	.createServer((req, res) => {
+		const file = req.url === "/" ? "index.html" : req.url;
+		const filePath = path.join(__dirname, "public", file);
+		fs.readFile(filePath, (error, content) => {
+			if (error) throw error;
+			res.end(content);
+		});
+		if (req.url === "/contato") return res.end("<h1>Contato</h1>");
+	})
+	.listen(5000, () => console.log("Server is runing..."));
